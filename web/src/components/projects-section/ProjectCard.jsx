e7720272e3f68268a22afd512c25d8fa751675d9
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-22 22:02:27 +0100

    add styles

--- web/src/components/projects-section/ProjectCard.jsx
+++ web/src/components/projects-section/ProjectCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
-function ProjectCard({name, description, image, order}) {
+function ProjectCard({name, architecture, description, image, order, tech1, tech2, tech3}) {
+
   return (
-    <div className='p-10 md: grid grid-cols-2 mt-20'>
-      <figure className=' aspect-video'>
-        <img src={image} alt="" className='h-full object-cover animate-show'/>
+    <div className='p-10 md:grid grid-cols-2 items-center'>
+      <figure className={`aspect-video ${order === 'last' ? 'md:order-last' : 'md:order-first'} relative`}>
+        <img src={image} alt={`captura de pantalla del proyecto ${name}`} className='h-full object-cover animate-show'/>
       </figure>
-      <article>
-        <h3 className='md: text-3xl'>{name}</h3>
-        <p>{description}</p>
+      <article className='space-y-4 p-10'>
+        <h3 className='md:text-3xl'>{name}<br></br>{architecture}</h3>
+        <p className='mt-8'>{description}</p>
       </article>
     </div>
     
   )
+}
+
+ProjectCard.defaultProps = {
+  className: ''
 }
 
 export default ProjectCard
--- web/src/components/projects-section/ProjectsSection.jsx
+++ web/src/components/projects-section/ProjectsSection.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import meImg from '../../assets/me-transp.png';
+import grimm from '../../assets/grimm.jpg';
 
 function ProjectsSection() {
   return (
     <section className=''>
-      <h2 className='space-y-4 mt-8 text-4xl md:text-6xl'>Mis ultimos proyectos</h2>
+      <h2 className='md:mb-20 text-4xl md:text-6xl'>Mis ultimos proyectos</h2>
       <ProjectCard 
-        name = "React shelter : Single Page Application" 
-        description="" 
-        image={meImg} />
+        name = "🐾 Fur-Ever Friends "
+        architecture="Plataforma de Adopción de Mascotas (SPA con React)"
+        description={<>Tras desarrollar la primera versión de Fur-Ever Friends con server-side rendering (HBS), decidí modernizar la plataforma creando una <b>Single Page Application (SPA)</b> con React, optimizando la experiencia del usuario con una navegación más fluida y rápida.</>} 
+        image={grimm}
+        tech1={meImg}
+        tech2={meImg}
+        tech3={meImg} />
       <ProjectCard 
-        name="Shelter Express : Server Side Rendering" 
+        name="🐾 Shelter Express : Server Side Rendering" 
         description="" 
-        image={meImg} 
-        order="2" />
+        image={grimm} 
+        order="last" />
       <ProjectCard 
-        name="Grimm Gate : juego javascript con canvas " 
-        description="" 
-        image={meImg} />
+        name="🎮 Grimm Gate : Juego con JavaScript y Canvas" 
+        description={<>Grimm Gate es un videojuego de plataformas en 2D, inspirado en Hollow Knight, desarrollado con JavaScript y Canvas API. El jugador controla a un personaje que evoluciona a medida que derrota enemigos, mejorando sus habilidades mientras enfrenta un desafío progresivo y dinámico.</>} 
+        image={grimm} />
       
     </section>
   )
