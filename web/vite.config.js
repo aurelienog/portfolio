086ad971313b9f252d3e32680e71e3bad4d01c33
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-27 21:00:46 +0100

    fix web

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,9 +5,5 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tailwindcss()], 
-  base: "/", // Asegura que el deploy se sirva desde la raíz
-  build: {
-    outDir: "dist",
-  },
+  plugins: [svgr(), react(), tailwindcss()]
 })
