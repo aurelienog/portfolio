be571231c059b16d9820e255f8d90da0e86d748f
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-27 21:42:28 +0100

    change icons web

--- web/src/components/footer/Footer.jsx
+++ web/src/components/footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import github from '../../assets/github.svg';
+import linkedIn from '../../assets/linkedin.svg';
+import NeonIcon from '../neon-icon/NeonIcon';
 
 function Footer() {
   return (
     <footer className='bg-black text-white/90 p-8'>
-
+      <ul className='flex justify-center gap-6 p-4 w-[80%] mx-auto'>
+        <li><NeonIcon icon={github} url="https://github.com/aurelienog"/></li>
+        <li><NeonIcon icon={linkedIn} url="https://www.linkedin.com/in/aurelie-nogueira"/></li>
+      </ul>
 
       <p className="text-xs text-center text-gray-400">&copy; 2025 Aurélie Nogueira. Todos los derechos reservados. <br></br>
       Ilustraciones por <a href="https://www.freepik.com" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-300">Freepik</a>.</p>
--- web/src/components/neon-icon/NeonIcon.jsx
+++ web/src/components/neon-icon/NeonIcon.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function NeonIcon({Icon, url}) {
+function NeonIcon({icon, url}) {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer" className='inline-block transition-colors transition-transform transition-outline duration-300 ease-in-out rounded-full aspect-square bg-gray-900 content-center text-white/60 hover:scale-125 hover:text-purple-500 w-12 cursor-pointer mx-auto hover:outline-2 hover:shadow-[0_0_5px_theme(colors.purple.200),_0_0_20px_theme(colors.purple.700)]'>
-      <Icon fill="currentColor" className="w-[70%] h-auto mx-auto "/>
+      <img src = {`${icon}`} className="w-[70%] h-auto mx-auto "/>
     </a>
   )
 }
