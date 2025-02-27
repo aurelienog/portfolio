8c9c26a99ef699ffb8573a088f0ad715f669dc84
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-27 14:31:25 +0100

    fixed navbar

--- web/src/components/navbar/Navbar.jsx
+++ web/src/components/navbar/Navbar.jsx
@@ -5,21 +5,21 @@ import MenuIcon from "../../assets/menu.svg?react"
 function Navbar() {
   return (
 
-      <nav className='sticky top-0 font-bold font-bebas-neue text-2xl px-8 uppercase md:w-[80%] max-w-screen-xl  bg-background z-50 h-[var(--header-height)] flex justify-between md:grid items-center grid-cols-[auto_2fr] lg:grid-cols-[2fr_4fr] mx-auto'>
+      <nav className='flex justify-around sticky top-0 font-bold font-bebas-neue text-2xl uppercase md:w-[80%] max-w-screen-xl bg-background z-50 h-[var(--header-height)] md:grid items-center grid-cols-[auto_2fr] lg:grid-cols-[2fr_4fr] mx-auto'>
         <div className='hover:underline hover:underline-offset-4 decoration-4'>
           <Link to="/">Aurélie Nogueira</Link>
         </div>
-        <div>
-          <label htmlFor='menu' className='md:hidden'>
+
+          <label htmlFor='menu' className='md:hidden z-20'>
             <MenuIcon/>
           </label>
           <input id='menu' type='checkbox' className='hidden peer'/>
-          <ul className='hidden md:flex justify-self-end gap-32'>
+          <ul className='fixed pt-30 inset-0 space-y-8 text-center md:space-y-0 md:p-0 bg-pink-300 md:bg-background md:relative md:items-center md:flex md:justify-self-end md:gap-32 [clip-path:circle(0%_at_top_right)] peer-checked:[clip-path:circle(60%_at_top_right)] md:[clip-path:circle(150%_at_top_right)] transition-[clip-path] duration-500'>
             <li className='hover:underline underline-offset-4 decoration-4'><Link to="/about">01_ Sobre Mí</Link></li>
             <li className='hover:underline underline-offset-4 decoration-4'><Link to="/about">02_ Mis proyectos</Link></li>
             <li className='hover:underline underline-offset-4 decoration-4'><Link to="/about">03_ Contáctame</Link></li>
           </ul>
-        </div>
+
         
       </nav>
 
