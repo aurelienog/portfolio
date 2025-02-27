8636c8b4385e787c265d420341d58af26becf32a
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-27 21:15:18 +0100

    config web

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Navbar from "./components/navBar/Navbar"
+import Navbar from "./components/navbar/Navbar"
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import Footer from "./components/footer/Footer"
@@ -10,7 +10,7 @@ function App() {
     <div className=" bg-background [--header-height:6rem]">
       <Navbar/>
       <Routes>
-        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/home?" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
       </Routes>
       <Footer/>
--- web/src/services/base-api.js
+++ web/src/services/base-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'https://aurelie-nogueira.vercel.app/api/v1'
+  baseURL: 'http://localhost:3000/api/v1'
 })
 
 export default http;
