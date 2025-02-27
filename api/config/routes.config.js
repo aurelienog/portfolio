bcab821d6ceb656a733608bc9f4d4f0b1bf337c4
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-28 00:22:02 +0100

    fix cors

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -1,7 +1,7 @@
 const api = require('express').Router();
 const common = require('../controllers/common.controller');
 
-api.get("/", common.home);
+
 api.post("/", common.mailer)
 
 module.exports = api;
--- web/src/services/base-api.js
+++ web/src/services/base-api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'https://portfolio-server-rho-amber.vercel.app/api/v1'
-})
+  baseURL: "https://portfolio-server-rho-amber.vercel.app/api/v1",
+  withCredentials:true
+});
 
-export default http;
+export default http; 
