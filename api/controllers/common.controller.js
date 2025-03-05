41e86f4082c6173add8d737738256c8f6ef7a4de
Aurelie Nogueira <aurelienog@gmail.com>
2025-03-05 18:45:37 +0100

    fixed : nodemailer errors

--- api/controllers/common.controller.js
+++ api/controllers/common.controller.js
@@ -5,8 +5,14 @@ module.exports.home = ((req, res, next) => {
   res.status(200).send();
 });
 
-module.exports.mailer = ((req, res, next) => {
-  console.log(req.body)
-  mailer.sendMail(req.body);
-  res.status(200).send();
-})
+module.exports.mailer = async (req, res, next) => {
+  console.log(req.body);
+  try {
+    await mailer.sendMail(req.body);
+    res.status(200).send({ message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send({ message: "Error sending email" });
+  }
+  
+}
