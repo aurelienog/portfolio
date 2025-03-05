d8dd8c18022b255b2232d93908b23784a98b2cf5
Aurelie Nogueira <aurelienog@gmail.com>
2025-03-05 13:27:55 +0100

    feat : connect to api

--- web/src/services/base-api.js
+++ web/src/services/base-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: "https://portfolio-server-rho-amber.vercel.app/api/v1",
-  withCredentials:true
+  withCredentials:true,
+  baseURL: "https://portfolio-api-umber-theta.vercel.app/api/v1"
 });
 
 export default http; 
