9ae0d444fcb4f021b9b9f50f7bdbfe0f07eb1da5
Aurelie Nogueira <aurelienog@gmail.com>
2025-03-26 16:00:39 +0100

    fixed splash animation

--- web/src/components/splash-icon/SplashIcon.jsx
+++ web/src/components/splash-icon/SplashIcon.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function SplashIcon({icon, splash, name}) {
 
   return (
-    <div className='h-25 aspect-square relative flex items-center justify-center'>
-      <img src={splash} alt='' className=' opacity-85'/>
+    <div className='h-25 aspect-square relative flex items-center justify-center scale-0'>
+      <img src={splash} alt='' className='opacity-85'/>
       <img src={icon} alt={name} className='absolute h-[50%] object-cover'/>
     </div>
   )
