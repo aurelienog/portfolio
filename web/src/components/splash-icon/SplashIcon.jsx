a675f8fd2a63092f6c8473226ec07f80226f9aab
Aurelie Nogueira <aurelienog@gmail.com>
2025-03-27 14:00:54 +0100

    fix animacion mobile

--- web/src/components/splash-icon/SplashIcon.jsx
+++ web/src/components/splash-icon/SplashIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SplashIcon({icon, splash, name}) {
 
   return (
-    <div className='h-25 aspect-square relative flex items-center justify-center scale-0'>
+    <div className='h-25 aspect-square relative flex items-center justify-center md:scale-0'>
       <img src={splash} alt='' className='opacity-85'/>
       <img src={icon} alt={name} className='absolute h-[50%] object-cover'/>
     </div>
