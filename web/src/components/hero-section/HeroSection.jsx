9131c5aeae523cfe67ec17aa4d61a03786e533a2
Aurelie Nogueira <aurelienog@gmail.com>
2025-07-08 13:54:13 +0200

    update about page

--- web/src/components/hero-section/HeroSection.jsx
+++ web/src/components/hero-section/HeroSection.jsx
@@ -11,7 +11,7 @@ function HeroSection() {
         <SplashBackground image={meImg}/>
         <img src={extraSplash} alt="" className='hidden md:block absolute w-32 -bottom-48 right-12 starting:opacity-0 transition-opacity delay-600 duration-700 ease-in-out opacity-100 [aria-hidden:"true"]'/>
       </figure>
-      <article className='mt-8'>
+      <article>
         <h1 className='mb-0'>Hola ! Soy Aurélie </h1>
         <p className='mt-8 mb-12 text-xl md:text-2xl'> Desarrolladora Full Stack enfocada en crear interfaces rápidas, accesibles y con una experiencia de usuario óptima.</p>
         <Link to={{
--- web/src/pages/AboutPage.jsx
+++ web/src/pages/AboutPage.jsx
@@ -1,15 +1,20 @@
 import PageLayout from '../components/layout/PageLayout';
+import { Link } from "react-router-dom";
+
 function AboutPage() {
   return (
     <PageLayout>
       <section className="mx-auto">
-        <h1>Mi viaje hacia la programación</h1>
+        <h1 className='text-[54px] md:text-9xl'>Mi viaje hacia la programación</h1>
         <div className='md:grid grid-cols-[2fr_1fr]'>
         <article className='space-y-8'>
-          <p>Durante varios años trabajé en la gestión de apartamentos turísticos, desarrollando habilidades de organización, autonomía y atención al detalle. Con el tiempo, descubrí un gran interés por el desarrollo web.</p>
-          <p>Me formé a través de un  <b className='highlight'>bootcamp full stack </b> y seguí aprendiendo por mi cuenta, profundizando en tecnologías como <b className='highlight'>Next.js</b> y <b className='highlight'>TypeScript</b>. <span className='block'>Como primer proyecto freelance, diseñé y desarrollé un sitio web completo para un artesano francés, abordando tanto el rendimiento como la <b className='highlight'>accesibilidad</b> y el <b className='highlight'>SEO</b>.</span></p>
-          <p>Hoy, estoy en búsqueda de mi primera oportunidad profesional como desarrolladora Full Stack, donde pueda aplicar lo aprendido, aportar soluciones y seguir creciendo en un entorno colaborativo.</p>
+          <p>Siempre me atrajo el mundo de la tecnología, pero durante mucho tiempo pensé que no era un camino accesible para mí.</p> <p>En 2024, decidí dar el paso hacia el desarrollo web y me formé a través de un <b className='highlight'>bootcamp full stack</b> basado en el stack <b className='highlight'>MERN</b>. Comenzamos desde los fundamentos de JavaScript (JS Vanilla y lógica de programación), y poco a poco fuimos avanzando hasta trabajar con React, Node.js, Express y MongoDB.</p>
+          <p>Al finalizar, seguí aprendiendo por mi cuenta, profundizando en tecnologías como <b className='highlight'>Next.js</b> y <b className='highlight'>TypeScript</b>. Como primer proyecto freelance, diseñé y desarrollé un sitio web completo para un artesano francés, abordando tanto el <b className='highlight'>rendimiento</b> como la <b className='highlight'>accesibilidad</b> y el <b className='highlight'>SEO</b>.</p>
+          <p>Hoy, estoy en búsqueda de mi primera oportunidad profesional como desarrolladora Full Stack, donde pueda aplicar lo aprendido, aportar soluciones y seguir creciendo en un <b className='highlight'>entorno colaborativo</b>.</p>
           <p><b className='highlight'>Vivo en Bilbao</b>, pero estoy abierta al trabajo en remoto si el proyecto lo permite.  <b className='highlight'>Habló francés, español e inglés</b>, lo que me permite integrarme fácilmente en equipos multilingües.</p>
+          <p>Si crees que podría encajar en tu equipo, 
+            <Link to={{ pathname: '/home', hash: "#contact" }} className='md:text-xl italic underline-offset-4 underline text-accent font-bold'> no dudes en escribirme</Link>
+            . Muchas gracias por llegar hasta aquí</p>
         </article>
         <figure>
 
