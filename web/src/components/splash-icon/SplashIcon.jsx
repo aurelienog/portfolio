d61cb43437d42038ae668cca41f227ccee1b4db4
Aurelie Nogueira <aurelienog@gmail.com>
2025-07-15 12:30:20 +0200

    add local fonts

--- web/src/components/splash-icon/SplashIcon.jsx
+++ web/src/components/splash-icon/SplashIcon.jsx
@@ -2,7 +2,7 @@ function SplashIcon({ name, icon, splash, height }) {
 
   return (
     <div className={`${height} aspect-square relative flex items-center justify-center`}>
-      <img src={splash} alt='' className='opacity-85'/>
+      <img src={splash} alt='' className='opacity-85' style={{ width: '100%', maxWidth: '100px', height: 'auto' }}/>
       <img src={icon} alt={name} className='absolute h-[50%] object-cover'/>
     </div>
   )
